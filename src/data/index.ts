6646370be2c7dc4c48d9111c1a16764081c61b7c
Tharun <tharungowda30115@gmail.com>
2025-03-10 01:11:56 +0530

    edit

--- src/data/index.ts
+++ src/data/index.ts
@@ -36,21 +36,21 @@ export const navItems = [
 export const reviews = [
 	{
 		content:
-			"I've owned several luxury watches, but my Reddy watch stands out from the rest. The craftsmanship is impeccable, and the attention to detail is truly remarkable. It's a timeless piece that never fails to garner compliments. I'm proud to be a Reddy watch owner",
-		name: "David T",
-		date: "Feb 22, 2023",
+			"I've owned several luxury watches, but my Vox watch stands out from the rest. The craftsmanship is impeccable, and the attention to detail is truly remarkable. It's a timeless piece that never fails to garner compliments. I'm proud to be a Vox watch owner",
+		name: "Abhi",
+		date: "Feb 22, 2025",
 	},
 	{
 		content:
-			"I've been wearing my Reddy watch for over a year now, and I'm extremely impressed with its performance. Not only does it look stunning, but it also keeps time flawlessly. The quality and reliability of this watch are unmatched. It's become an essential part of my everyday style",
-		name: "Emma Turner",
-		date: "Feb 17, 2023",
+			"I've been wearing my Vox watch for over a year now, and I'm extremely impressed with its performance. Not only does it look stunning, but it also keeps time flawlessly. The quality and reliability of this watch are unmatched. It's become an essential part of my everyday style",
+		name: "Tejas Reddy",
+		date: "Feb 17, 2025",
 	},
 	{
 		content:
-			"As a watch collector, I appreciate watches that blend elegance with functionality. My Reddy watch does exactly that. The design is sleek and sophisticated, and the automatic movement keeps perfect time. I've received numerous compliments on its unique design and [...]",
-		name: "Emily Bennett",
-		date: "Feb 15, 2023",
+			"As a watch collector, I appreciate watches that blend elegance with functionality. My Vox watch does exactly that. The design is sleek and sophisticated, and the automatic movement keeps perfect time. I've received numerous compliments on its unique design and [...]",
+		name: "Jeevan",
+		date: "Feb 15, 2025",
 	},
 ];
 
@@ -59,37 +59,37 @@ export const features = [
 		image: w1,
 		title: "Superior Craftsmanship",
 		description:
-			"Each Reddy watch is meticulously crafted with precision and attention to detail, reflecting the artistry of expert watchmakers.",
+			"Each Vox watch is meticulously crafted with precision and attention to detail, reflecting the artistry of expert watchmakers.",
 	},
 	{
 		image: w2,
 		title: "Attention to Detail",
 		description:
-			"Every aspect of the Reddy watch showcases meticulous attention to detail, from the finely engraved logo to the perfectly proportioned.",
+			"Every aspect of the Vox watch showcases meticulous attention to detail, from the finely engraved logo to the perfectly proportioned.",
 	},
 	{
 		image: w3,
 		title: "Elegant and Timeless Design",
 		description:
-			"The Reddy watch features an elegant and timeless design, characterized by clean lines, minimalistic dials, and a sophisticated aesthetic.",
+			"The Vox watch features an elegant and timeless design, characterized by clean lines, minimalistic dials, and a sophisticated aesthetic.",
 	},
 	{
 		image: w4,
 		title: "Comfortable Fit",
 		description:
-			"Designed with wearer comfort in mind, the Reddy watch offers a comfortable fit on the wrist, making it suitable for prolonged wear.",
+			"Designed with wearer comfort in mind, the Vox watch offers a comfortable fit on the wrist, making it suitable for prolonged wear.",
 	},
 	{
 		image: w5,
 		title: "Water-Resistance",
 		description:
-			"The Reddy watch is water-resistant, providing protection against water splashes and allowing you to wear it confidently in situations.",
+			"The Vox watch is water-resistant, providing protection against water splashes and allowing you to wear it confidently in situations.",
 	},
 	{
 		image: w6,
 		title: "Interchangeable Straps",
 		description:
-			"Customize your Reddy watch to match your personal style and mood. The watch comes with interchangeable straps, allowing you switch.",
+			"Customize your Vox watch to match your personal style and mood. The watch comes with interchangeable straps, allowing you switch.",
 	},
 ];
 
